a37154ecb76c6b6397087261fd4457e6a0d51b3f
Jade Kinomoto <jadekinomoto@Jades-MacBook-Pro.local>
2018-08-08 10:42:34 -0700

    Some basic app functionality

---
+++ artist_service.py
@@ -0,0 +1,32 @@
+artist_info = {
+    'Glass Animals': {
+        'image': 'GlassAnimals.jpg',
+        'spotify_link': '4yvcSjfu4PC0CYQyLy4wSq',
+        'youtube_link': 'nOHEuhJf7nA',
+        'twitter_link': 'GlassAnimals',
+        },
+    'Drake': {
+        'image': 'Drake.jpg',
+        'spotify_link': '3TVXtAsR1Inumwj472S9r4',
+        'youtube_link': 'uxpDa-c-4Mc',
+        'twitter_link': 'Drake',
+        },
+    'Chance The Rapper': {
+        'image': 'ChanceTheRapper.jpg',
+        'spotify_link': '1anyVhU62p31KFi8MEzkbf',
+        'youtube_link': 'DVkkYlQNmbc',
+        'twitter_link': 'chancetherapper',
+        },
+    'Khalid': {
+        'image': 'Khalid.jpg',
+        'spotify_link': '6LuN9FCkKOj5PcnpouEgny',
+        'youtube_link': 'by3yRdlQvzs',
+        'twitter_link': 'thegreatkhalid',
+        },
+    'Lorde': {
+        'image': 'Lorde.jpg',
+        'spotify_link': '4yvcSjfu4PC0CYQyLy4wSq',
+        'youtube_link': 'LONIThD7vO8',
+        'twitter_link': 'lorde',
+        }
+}
